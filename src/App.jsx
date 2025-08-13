e517fd4edf7907126650f738d5d7b5c2c64e517d
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-08-13 14:07:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import Multiselect from './Multiselect'
+
+function App() {
+
+  return <Multiselect />;
+}
+
+export default App
---
+++ src/Multiselect/index.jsx
@@ -0,0 +1,5 @@
+const Multiselect = () => {
+    return <div className="text-3xl font-bold text-red-50">Multiselect</div>;
+};
+
+export default Multiselect;
